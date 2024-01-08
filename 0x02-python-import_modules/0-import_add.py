b06b1794ac833eab7baa3e03ca7adb671eb206a1
Nasserb2 <abdennasser.bedroune@gmail.com>
2024-01-08 08:07:45 -0800

    Sda3

--- 0x02-python-import_modules/0-import_add.py
+++ 0x02-python-import_modules/0-import_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 a = 1
 b = 2
-add_0 = __import__("add_0").add
-print("{:d} + {:d} = {:d}\n".format(a, b, add(a, b)))
+add_0 = __import__("0-add").add
+print("{:d} + {:d} = {:d}\n".format(a, b, add_0(a, b)))
 
